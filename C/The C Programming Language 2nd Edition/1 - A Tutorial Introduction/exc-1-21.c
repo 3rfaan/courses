97aabfabb8eb7bf9a5e722992c64eabe8b50a0ec
3rfan <28968837+3rfaan@users.noreply.github.com>
2022-07-02 19:39:41 +0200

    Added Exercise  1-21

---
+++ C/The C Programming Language 2nd Edition/1 - A Tutorial Introduction/exc-1-21.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define TABSIZE 8
+
+/* Write a program 'entab' that replaces strings of blanks by the minimum
+number of tabs and blanks to achieve the same spacing. Use the same tab
+stops as for 'detab'. When either a tab or a single blank would suffice
+to reach a tab stop, which should be given preference? */
+
+int main(void) {
+    int c, blanks, tabs, pos;
+
+    blanks = 0;  // # of blanks necessary
+    tabs = 0;    // # of tabs necessary
+
+    for (pos = 1; (c = getchar()) != EOF; ++pos) {
+        if (c == ' ') {
+            if (pos % TABSIZE != 0)
+                ++blanks;
+            else {
+                blanks = 0;
+                ++tabs;
+            }
+        } else {
+            while (tabs > 0) {
+                putchar('\t');
+                --tabs;
+            }
+            if (c == '\t')
+                blanks = 0;
+            else
+                for (; blanks > 0; --blanks) putchar(' ');
+            putchar(c);
+            if (c == '\n')
+                pos = 0;
+            else if (c == '\t')
+                pos += (TABSIZE - (pos - 1) % TABSIZE) - 1;
+        }
+    }
+}
