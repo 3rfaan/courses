4b4685e4884083dadea0baed3a5d467a52e0f9b1
3rfan <28968837+3rfaan@users.noreply.github.com>
2022-07-08 01:39:41 +0200

    Added Exercise 2-3

---
+++ C/The C Programming Language 2nd Edition/2 - Types, Operators, and Expressions/exc-2-3.c
@@ -0,0 +1,36 @@
+/* Write the function htoi(s), which converts a string of hexdecimal digits (including
+an optional 0x or 0X) into its equivalent integer value.
+The allowable digits are 0 through 9, a through f, and A through F. */
+
+#include <ctype.h>
+#include <stdio.h>
+
+unsigned long htoi(const char s[]);
+
+int main(void) {
+    printf("%ld\n", htoi("0xFA9c"));
+    printf("%ld\n", htoi("0xFFFF"));
+    printf("%ld\n", htoi("0x1111"));
+    printf("%ld\n", htoi("0xBCDA"));
+    return 0;
+}
+
+unsigned long htoi(const char s[]) {
+    int i;
+    unsigned long n;
+
+    i = 0;
+    n = 0;
+
+    for (i = 0; s[i] != '\0'; ++i) {
+        int c = tolower(s[i]);
+
+        if (c == '0' && tolower(s[i + 1]) == 'x')
+            i++;
+        else if (c >= '0' && c <= '9')
+            n = 16 * n + (c - '0');
+        else if (c >= 'a' && c <= 'f')
+            n = 16 * n + (c - 'a' + 10);
+    }
+    return n;
+}
