d5fd02e00bb76e3e7c6a75880bff9e579b917027
3rfaan <28968837+3rfaan@users.noreply.github.com>
2022-07-07 17:24:58 +0200

    Added atoi example in main folder

---
+++ C/The C Programming Language 2nd Edition/atoi.c
@@ -0,0 +1,9 @@
+/* atoi: convert s to integer */
+int atoi(char s[]) {
+    int i, n;
+
+    n = 0;
+    for (i = 0; s[i] >= '0' && s[i] <= '9'; ++i)
+        n = 10 * n + (s[i] - '0');
+    return n;
+}
