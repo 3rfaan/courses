e5b869cb570af513ef7a0be14d372c1e2840747d
3rfan <28968837+3rfaan@users.noreply.github.com>
2022-07-07 22:50:20 +0200

    Deleted files from main

--- C/The C Programming Language 2nd Edition/atoi.c
+++
@@ -1,9 +0,0 @@
-/* atoi: convert s to integer */
-int atoi(char s[]) {
-    int i, n;
-
-    n = 0;
-    for (i = 0; s[i] >= '0' && s[i] <= '9'; ++i)
-        n = 10 * n + (s[i] - '0');
-    return n;
-}
--- C/The C Programming Language 2nd Edition/lower.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-/* lower: convert c to lower case; ASCII only */
-int lower(int c) {
-    if (c >= 'A' && c <= 'Z') {
-        return c + 'a' - 'A';
-    } else
-        return c;
-}
