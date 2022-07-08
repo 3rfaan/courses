21b0afcd29a6b75263f3fada0e91b6a98099665f
3rfan <28968837+3rfaan@users.noreply.github.com>
2022-07-08 02:42:32 +0200

    Added solution of C Answer Book

--- C/The C Programming Language 2nd Edition/2 - Types, Operators, and Expressions/exc-2-3.c
+++ C/The C Programming Language 2nd Edition/2 - Types, Operators, and Expressions/exc-2-3.c
@@ -5,13 +5,22 @@ The allowable digits are 0 through 9, a through f, and A through F. */
 #include <ctype.h>
 #include <stdio.h>
 
+#define YES 1
+#define NO 0
+
 unsigned long htoi(const char s[]);
+int htoiV2(char s[]);
 
 int main(void) {
     printf("%ld\n", htoi("0xFA9c"));
     printf("%ld\n", htoi("0xFFFF"));
     printf("%ld\n", htoi("0x1111"));
     printf("%ld\n", htoi("0xBCDA"));
+    putchar('\n');
+    printf("%d\n", htoiV2("0xFA9c"));
+    printf("%d\n", htoiV2("0xFFFF"));
+    printf("%d\n", htoiV2("0x1111"));
+    printf("%d\n", htoiV2("0xBCDA"));
     return 0;
 }
 
@@ -23,11 +32,40 @@ unsigned long htoi(const char s[]) {
         int c = tolower(s[i]);
 
         if (c == '0' && tolower(s[i + 1]) == 'x')
-            i++;
+            ++i;
         else if (c >= '0' && c <= '9')
             n = 16 * n + (c - '0');
         else if (c >= 'a' && c <= 'f')
             n = 16 * n + (c - 'a' + 10);
+    }
+    return n;
+}
+
+// This is the solution of the 'C Answer Book':
+
+/* htoi: convert hexdecimal string s to integer */
+int htoiV2(char s[]) {
+    int hexdigit, i, inhex, n;
+
+    i = 0;
+    if (s[i] == '0') {  // skip optional 0x or 0X
+        ++i;
+        if (s[i] == 'x' || s[i] == 'X')
+            ++i;
+    }
+
+    n = 0;
+    inhex = YES;
+    for (; inhex == YES; ++i) {
+        if (s[i] >= '0' && s[i] <= '9')
+            hexdigit = s[i] - '0';
+        else if (s[i] >= 'a' && s[i] <= 'f')
+            hexdigit = s[i] - 'a' + 10;
+        else if (s[i] >= 'A' && s[i] <= 'F')
+            hexdigit = s[i] - 'A' + 10;
+        else
+            inhex = NO;  // not a valid hexadecimal digit
+        if (inhex == YES) n = 16 * n + hexdigit;
     }
     return n;
 }
