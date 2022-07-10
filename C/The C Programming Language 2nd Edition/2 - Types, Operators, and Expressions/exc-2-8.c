93eb7d06e1615254915bc9605b93bc61869c5f54
3rfan <28968837+3rfaan@users.noreply.github.com>
2022-07-10 02:44:48 +0200

    Added uintsize function and more explanations

--- C/The C Programming Language 2nd Edition/2 - Types, Operators, and Expressions/exc-2-8.c
+++ C/The C Programming Language 2nd Edition/2 - Types, Operators, and Expressions/exc-2-8.c
@@ -3,14 +3,15 @@ rotated to the right by n bit positions. */
 
 #include <stdio.h>
 
-#define UINT_S 8  // size of unsigned integer (8 bytes)
+#define UINT_S 8  // pretending size of unsigned integer (8 bits)
 
 unsigned int rightrot(unsigned int x, int n);
+unsigned int uintsize(void);
 
 /*
 Right Rotate x = 229 = 1110 0101 by n = 3
 Mask: 0000 0111
-Pretending size of uint: 8 bytes
+Pretending size of uint: 8 bits
 
 1) x >> 3 ==> 1110 0101 >> 3 ==> 0001 1100
 2) x & mask ==> 1110 0101 & 0000 0111 ==> 0000 0101
@@ -19,7 +20,7 @@ Pretending size of uint: 8 bytes
 */
 
 int main(void) {
-    printf("%u\n", rightrot(229, 3));
+    printf("UINT after rotating: %u\n", rightrot(229, 3));
     return 0;
 }
 
@@ -27,4 +28,18 @@ unsigned int rightrot(unsigned int x, int n) {
     unsigned int mask = ~(~0 << n);  // ~(1111 1111 << 3) ==> ~1111 1000 ==> 0000 0111
 
     return x >> n | ((x & mask) << (UINT_S - n));
+    /*
+    if we wanted to use the real size of an UINT (on my system 32 bits) we use uintsize function:
+     return x >> n | ((x & mask) << (uintsize() - n));
+    */
+}
+
+/* returns the size in bits of an UINT */
+unsigned int uintsize(void) {
+    int i;
+    unsigned int v = (unsigned int)~0;
+
+    for (i = 1; (v = v >> 1) > 0; ++i)
+        ;
+    return i;
 }
