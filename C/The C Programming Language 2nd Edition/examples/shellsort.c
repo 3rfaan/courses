aec033270de1dc4dbf8c570e42ac60d0b927182b
3rfan <28968837+3rfaan@users.noreply.github.com>
2022-07-15 01:28:32 +0200

    Changed i++ to ++i in shellsort func for consistency

--- C/The C Programming Language 2nd Edition/examples/shellsort.c
+++ C/The C Programming Language 2nd Edition/examples/shellsort.c
@@ -26,7 +26,7 @@ void shellsort(int v[], int n) {
     int gap, i, j, temp;
 
     for (gap = n / 2; gap > 0; gap /= 2)
-        for (i = gap; i < n; i++)
+        for (i = gap; i < n; ++i)
             for (j = i - gap; j >= 0 && v[j] > v[j + gap]; j -= gap) {
                 temp = v[j];
                 v[j] = v[j + gap];
