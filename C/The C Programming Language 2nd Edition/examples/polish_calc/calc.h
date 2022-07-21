ee72550eb9e8dec86dec0d185e2fcd88ae062bc6
3rfan <28968837+3rfaan@users.noreply.github.com>
2022-07-21 22:11:13 +0200

    Added extern keyword in calc.h for learning purposes, it's not necessary

--- C/The C Programming Language 2nd Edition/examples/polish_calc/calc.h
+++ C/The C Programming Language 2nd Edition/examples/polish_calc/calc.h
@@ -1,7 +1,7 @@
 #define NUMBER '0'
 
-int getop(char[]);
-void push(double);
-double pop(void);
-int getch(void);
-void ungetch(int);
+extern int getop(char[]);
+extern void push(double);
+extern double pop(void);
+extern int getch(void);
+extern void ungetch(int);
