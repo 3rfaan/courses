7ec87167617ea81c394fdcfe22c045a764495f8d
3rfan <28968837+3rfaan@users.noreply.github.com>
2022-07-24 23:27:23 +0200

    Added conditional inclusion macros

--- C/The C Programming Language 2nd Edition/examples/macros.h
+++ C/The C Programming Language 2nd Edition/examples/macros.h
@@ -1,7 +1,31 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /* dprint: Print an expression */
 #define dprint(expr) printf(#expr " = %g\n", expr) // dprint(x/y) --> printf("x/y = %g\n", x/y)
 
 /* paste: concatenates its two arguments */
 #define paste(front, back) front ## back // paste(name, 1) --> name1
+
+/* Make sure contents of file hdr.h are included only once: */
+#if !defined(HDR)
+#define HDR
+// contents of hdr.h go here
+#endif
+
+/* The first example of #if above could have been written: */
+#ifndef HDR
+#define HDR
+// contents of hdr.h go here
+#endif
+
+/* This sequence tests the name SYSTEM to decide which version of header to include: */
+#if SYSTEM == SYSV
+    #define HDR "sysv.h"
+#elif SYSTEM == BSD
+    #define HDR "bsd.h"
+#elif SYSTEM == MSDOS
+    #define HDR "msdos.h"
+#else
+    #define HDR "default.h"
+#endif
+// #include HDR
