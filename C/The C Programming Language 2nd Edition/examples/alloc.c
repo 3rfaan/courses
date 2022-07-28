d3f70ed4369df9fda8679c2b843c9070db97208a
3rfan <28968837+3rfaan@users.noreply.github.com>
2022-07-28 20:03:32 +0200

    Added alloc.c example

---
+++ C/The C Programming Language 2nd Edition/examples/alloc.c
@@ -0,0 +1,19 @@
+#define ALLOCSIZE 10000  // size of available space
+
+static char allocbuf[ALLOCSIZE];  // storage for alloc
+static char *allocp = allocbuf;   // next free position
+
+/* alloc: return pointer to n characters */
+char *alloc(int n) {
+    if (allocbuf + ALLOCSIZE - allocp >= n) {  // it fits
+        allocp += n;
+        return allocp - n;  // old p
+    } else                  // not enough room
+        return 0;
+}
+
+/* afree: free storage pointed to by p */
+void afree(char *p) {
+    if (p >= allocbuf && p < allocbuf + ALLOCSIZE)
+        allocp = p;
+}
