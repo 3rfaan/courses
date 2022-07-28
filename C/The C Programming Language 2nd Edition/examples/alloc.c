1cb10422826e763d09dd116054e7c9aada32dc61
3rfan <28968837+3rfaan@users.noreply.github.com>
2022-07-28 21:30:38 +0200

    Added falloc for allocating float numbers in allocbuf

--- C/The C Programming Language 2nd Edition/examples/alloc.c
+++ C/The C Programming Language 2nd Edition/examples/alloc.c
@@ -1,7 +1,10 @@
+
 #define ALLOCSIZE 10000  // size of available space
 
 static char allocbuf[ALLOCSIZE];  // storage for alloc
 static char *allocp = allocbuf;   // next free position
+
+int main(void) { return 0; }
 
 /* alloc: return pointer to n characters */
 char *alloc(int n) {
@@ -9,6 +12,15 @@ char *alloc(int n) {
         allocp += n;
         return allocp - n;  // old p
     } else                  // not enough room
+        return 0;
+}
+
+/* falloc: return pointer to n characters of type float */
+float *falloc(int n) {
+    if (allocbuf + ALLOCSIZE - allocp >= n) {
+        allocp += n;
+        return (float *)allocp - n;  // old p
+    } else                           // not enough room
         return 0;
 }
 
