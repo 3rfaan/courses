2523ac3255bfcf4e3e40470c67148f26b2ebdf16
3rfan <28968837+3rfaan@users.noreply.github.com>
2022-07-28 10:08:08 +0200

    Added array of chars and string constant as examples

--- C/The C Programming Language 2nd Edition/examples/strlen.c
+++ C/The C Programming Language 2nd Edition/examples/strlen.c
@@ -10,7 +10,10 @@ int strlen(char *s) {
 
 int main(void) {
     char *s = "Test String";
+    char a[] = "Another Test String";
 
     printf("String Length of \"%s\": %d\n", s, strlen(s));
+    printf("String Length of \"%s\": %d\n", a, strlen(a));
+    printf("String Length of \"%s\": %d\n", "In-line Test String", strlen("In-line Test String"));
     return 0;
 }
