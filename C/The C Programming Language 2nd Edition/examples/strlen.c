39b99a4d1a2ace9a9bdd9cd0718fd0c0fc78d177
3rfan <28968837+3rfaan@users.noreply.github.com>
2022-07-28 21:17:28 +0200

    Added version 2 of function strlen (strlenV2)

--- C/The C Programming Language 2nd Edition/examples/strlen.c
+++ C/The C Programming Language 2nd Edition/examples/strlen.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <stdio.h>
 
 /* strlen: return length of string s */
@@ -8,12 +9,25 @@ int strlen(char *s) {
     return n;
 }
 
+/* strlenV2: return length of string s; version 2 */
+ptrdiff_t strlenV2(char *s) {
+    char *p = s;
+
+    while (*p != '\0') p++;
+    return p - s;
+}
+
 int main(void) {
     char *s = "Test String";
     char a[] = "Another Test String";
 
-    printf("String Length of \"%s\": %d\n", s, strlen(s));
+    printf("\nString Length of \"%s\": %d\n", s, strlen(s));
     printf("String Length of \"%s\": %d\n", a, strlen(a));
     printf("String Length of \"%s\": %d\n", "In-line Test String", strlen("In-line Test String"));
+    putchar('\n');
+    printf("String Length of \"%s\": %td\n", s, strlenV2(s));
+    printf("String Length of \"%s\": %td\n", a, strlenV2(a));
+    printf("String Length of \"%s\": %td\n", "In-line Test String", strlenV2("In-line Test String"));
+    putchar('\n');
     return 0;
 }
