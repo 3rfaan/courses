4c2dec89a857061a3943644cc0704252d0f7f85a
3rfan <28968837+3rfaan@users.noreply.github.com>
2022-07-29 10:56:06 +0200

    Changed STR_BUFFER to BUFFER and string names 1 and 2 to s and t

--- C/The C Programming Language 2nd Edition/5 - Pointers and Arrays/exc-5-3.c
+++ C/The C Programming Language 2nd Edition/5 - Pointers and Arrays/exc-5-3.c
@@ -3,16 +3,16 @@ strcat(s,t) copies the string t to the end of s. */
 
 #include <stdio.h>
 
-#define STR_BUFFER 10000
+#define BUFFER 10000
 
 void strcat_m(char *, char *);
 
 int main(void) {
-    char s[STR_BUFFER] = "The C ";
-    char t[STR_BUFFER] = "Programming Language";
+    char s[BUFFER] = "The C ";
+    char t[BUFFER] = "Programming Language";
 
-    printf("String 1: %s\n", s);
-    printf("String 2: %s\n\n", t);
+    printf("String s: %s\n", s);
+    printf("String t: %s\n\n", t);
 
     strcat_m(s, t);
     printf("Cat Result: %s\n", s);
