4fd49a20e0034d1cade6437685163b98c5e10be5
3rfan <28968837+3rfaan@users.noreply.github.com>
2022-08-04 21:17:20 +0200

    Added find example

---
+++ C/The C Programming Language 2nd Edition/examples/find.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLINE 1000
+
+int getl(char *line, int max);
+
+/* find: print lines that match pattern from 1st arg */
+int main(int argc, char *argv[]) {
+    char line[MAXLINE];
+    int found = 0;
+
+    if (argc != 2)
+        printf("Usage: Find pattern\n");
+    else
+        while (getl(line, MAXLINE) > 0)
+            if (strstr(line, argv[1]) != NULL) {
+                found++;
+            }
+    printf("Found: %d matches\n", found);
+    return found;
+}
+
+/* getl: read a line into s, return length */
+int getl(char *s, int lim) {
+    int c;
+    char *t = s;  // beginning of string s
+
+    while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+        *s++ = c;
+    if (c == '\n') *s++ = c;
+    *s = '\0';
+    return s - t;
+}
