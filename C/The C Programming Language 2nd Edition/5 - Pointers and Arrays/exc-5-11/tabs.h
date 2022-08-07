805ee75d7d0ac3b914be0ec8ee0b8ca9a7b31a01
3rfan <28968837+3rfaan@users.noreply.github.com>
2022-08-07 22:34:16 +0200

    Added detab in tabs.h and Makefile

--- C/The C Programming Language 2nd Edition/5 - Pointers and Arrays/exc-5-11/tabs.h
+++ C/The C Programming Language 2nd Edition/5 - Pointers and Arrays/exc-5-11/tabs.h
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAXLINE 100 // max line size
-#define TABINC 8 // default tab increment size
+#define MAXLINE 100  // max line size
+#define TABINC 8     // default tab increment size
 #define YES 1
 #define NO 0
 
 void settab(int argc, char* argv[], char* tab);
 void entab(char* tab);
 int tabpos(int pos, char* tab);
+void detab(char* tab);
