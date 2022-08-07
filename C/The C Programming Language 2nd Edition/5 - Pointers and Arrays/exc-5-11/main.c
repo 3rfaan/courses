e508df4ecdca10fcce4ccd573a8cce07031d2385
3rfan <28968837+3rfaan@users.noreply.github.com>
2022-08-07 22:40:30 +0200

    Added exercise description in main

--- C/The C Programming Language 2nd Edition/5 - Pointers and Arrays/exc-5-11/main.c
+++ C/The C Programming Language 2nd Edition/5 - Pointers and Arrays/exc-5-11/main.c
@@ -1,3 +1,6 @@
+/* Modify the programs entab and detab (written as exercises in Chapter 1) to accept a list of tab stops
+as arguments. Use the default tab settings if there are no arguments. */
+
 #include "tabs.h"
 
 /* replace strings of blanks with tabs */
