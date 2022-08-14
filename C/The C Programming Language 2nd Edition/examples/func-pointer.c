0b53aa7d5b75718153c03951f5fad6ea701d3923
3rfan <28968837+3rfaan@users.noreply.github.com>
2022-08-14 14:25:06 +0200

    Added Pointers to Functions example

--- C/The C Programming Language 2nd Edition/examples/func-pointer.c
+++ C/The C Programming Language 2nd Edition/examples/func-pointer.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define MAXLINES 5000  // max # of lines to be sorted
-#define MAXLEN 1000    // max length of any input line
 
 char *lineptr[MAXLINES];  // pointers to text lines
 
@@ -15,7 +15,7 @@ int numcmp(char *, char *);
 
 /* sort input lines */
 int main(int argc, char *argv[]) {
-    int nlines;       // number of input lines read
+    int nlines;       // # of input lines read
     int numeric = 0;  // 1 if numeric sort
 
     if (argc > 1 && strcmp(argv[1], "-n") == 0) numeric = 1;
@@ -30,67 +30,47 @@ int main(int argc, char *argv[]) {
     }
 }
 
-/* readlines: read input lines */
-int readlines(char *lineptr[], int maxlines) {
-    int len, nlines;
-    char *p, line[MAXLEN];
-
-    nlines = 0;
-    printf("Type some text..\n");
-    while ((len = getl(line, MAXLEN)) > 0)
-        if (nlines >= maxlines || (p = alloc(len)) == NULL)
-            return -1;
-        else {
-            line[len - 1] = '\0';  // delete newline
-            strcpy(p, line);
-            lineptr[nlines++] = p;
-        }
-    return nlines;
-}
-
-/* writelines: write output lines */
-void writelines(char *lineptr[], int nlines) {
-    printf("\n\nSorted Lines:\n");
-    while (nlines-- > 0)
-        printf("-> %s\n", *lineptr++);
-}
-
 /* qsort: sort v[left]...v[right] into increasing order */
 void qsort(void *v[], int left, int right,
            int (*comp)(void *, void *)) {
     int i, last;
     void swap(void *v[], int, int);
 
-    if (left >= right) return;           // do nothing if array contains fewer than two elements
-    swap(v, left, (left + right) / 2);   // move partition elem to v[0]
-    last = left;                         // "   "   "
-    for (i = left + 1; i <= right; i++)  // partition
+    if (left >= right) return;  // do nothing if array contains fewer than two elements
+    swap(v, left, (left + right) / 2);
+    last = left;
+    for (i = left + 1; i <= right; i++)
         if ((*comp)(v[i], v[left]) < 0) swap(v, ++last, i);
-    swap(v, left, last);  // restore partition elem
+    swap(v, left, last);
     qsort(v, left, last - 1, comp);
     qsort(v, last + 1, right, comp);
 }
 
-/* swap: interchange v[i] and v[j] */
-void swap(void *v[], int i, int j) {
-    int temp;
-
-    temp = v[i];
-    v[i] = v[j];
-    v[j] = temp;
+/* strcmp: return <0 if s < t, 0 if s == t, >0 if s > t; pointer version */
+int strcmpV1(char *s, char *t) {
+    for (; *s == *t; s++, t++)
+        if (*s == '\0') return 0;
+    return *s - *t;
 }
 
 /* numcmp: compare s1 and s2 numerically */
 int numcmp(char *s1, char *s2) {
     double v1, v2;
 
-    v1 = atof(s1);
-    v2 = atof(s2);
-
+    v1 = atof(s1), v2 = atof(s2);
     if (v1 < v2)
         return -1;
     else if (v1 > v2)
         return 1;
     else
         return 0;
+}
+
+/* swap: interchange v[i] and v[j] */
+void swap(void *v[], int i, int j) {
+    void *temp;
+
+    temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
 }
