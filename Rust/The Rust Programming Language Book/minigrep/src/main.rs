2cda1f16d028b396441f4de48f7fd1f7273b5cdb
3rfan <28968837+3rfaan@users.noreply.github.com>
2022-10-01 19:26:04 +0200

    Added I/O project: Command Line Program minigrep

---
+++ Rust/The Rust Programming Language Book/minigrep/src/main.rs
@@ -0,0 +1,18 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
