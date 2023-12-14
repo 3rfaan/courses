f0e3893e168841f9438dc7dc57ad93f83581dfb5
3rfaan <28968837+3rfaan@users.noreply.github.com>
2023-12-14 01:40:06 +0100

    Added data structures from Rustonomicon

---
+++ Rust/rustonomicon/data_structures/src/bin/mutable_slice.rs
@@ -0,0 +1,40 @@
+use std::mem;
+
+pub struct IterMut<'a, T: 'a>(&'a mut [T]);
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let slice = mem::take(&mut self.0);
+
+        if slice.is_empty() {
+            return None;
+        }
+
+        let (l, r) = slice.split_at_mut(1);
+
+        self.0 = r;
+
+        l.get_mut(0)
+    }
+}
+
+impl<'a, T> DoubleEndedIterator for IterMut<'a, T> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        let slice = mem::take(&mut self.0);
+
+        if slice.is_empty() {
+            return None;
+        }
+
+        let new_len = slice.len() - 1;
+        let (l, r) = slice.split_at_mut(new_len);
+
+        self.0 = l;
+
+        r.get_mut(0)
+    }
+}
+
+fn main() {}
---
+++ Rust/rustonomicon/data_structures/src/bin/singly_linked_list.rs
@@ -0,0 +1,33 @@
+#![allow(dead_code)]
+
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+pub struct LinkedList<T> {
+    head: Link<T>,
+}
+
+pub struct IterMut<'a, T: 'a>(Option<&'a mut Node<T>>);
+
+impl<T> LinkedList<T> {
+    fn iter_mut(&mut self) -> IterMut<T> {
+        IterMut(self.head.as_mut().map(|node| &mut **node))
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.take().map(|node| {
+            self.0 = node.next.as_mut().map(|node| &mut **node);
+            &mut node.elem
+        })
+    }
+}
+
+fn main() {}
