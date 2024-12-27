8c22376b9af4d26d78ce900e2f7e93f2b85fd337
3rfaan <zubi.arfan@gmail.com>
2024-12-27 01:53:38 +0100

    New Rust project: Build a programming language in Rust

---
+++ Rust/eldiro_v1/crates/eldiro/src/val.rs
@@ -0,0 +1,16 @@
+use std::fmt;
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Val {
+    Number(i32),
+    Unit,
+}
+
+impl fmt::Display for Val {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Number(n) => write!(f, "{}", n),
+            Self::Unit => write!(f, "Unit"),
+        }
+    }
+}
