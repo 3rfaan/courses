6af9c534ea37569a9cd37d0a4c5a0080798174d1
3rfaan <zubi.arfan@gmail.com>
2025-08-28 12:11:21 +0200

    Removed rustlings, will push updated version

--- Rust/rustlings/exercises/variables/variables1.rs
+++
@@ -1,9 +0,0 @@
-// variables1.rs
-// Make me compile!
-// Execute `rustlings hint variables1` or use the `hint` watch subcommand for a hint.
-
-
-fn main() {
-    let x = 5;
-    println!("x has the value {}", x);
-}
--- Rust/rustlings/exercises/variables/variables3.rs
+++
@@ -1,8 +0,0 @@
-// variables3.rs
-// Execute `rustlings hint variables3` or use the `hint` watch subcommand for a hint.
-
-
-fn main() {
-    let x: i32 = 7;
-    println!("Number {}", x);
-}
