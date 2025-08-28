3f71aa4b779b32d252bf59f071ea2efa52f0b9f4
3rfaan <zubi.arfan@gmail.com>
2025-08-28 16:39:29 +0200

    Hello, world chapter

---
+++ Go/Learn Go with Tests/hello/hello.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, "
+
+func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
+}
+
+func main() {
+	fmt.Println(Hello("world"))
+}
