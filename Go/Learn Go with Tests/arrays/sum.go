afe9a843d7e8c0cc9e4d3ad1a70863bcd1a8f562
3rfaan <zubi.arfan@gmail.com>
2025-08-28 20:16:19 +0200

    Added chapter arrays

--- Go/Learn Go with Tests/arrays/sum.go
+++ Go/Learn Go with Tests/arrays/sum.go
@@ -7,3 +7,17 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, n := range numbersToSum {
+		if len(n) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := n[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
--- Go/Learn Go with Tests/arrays/sum_test.go
+++ Go/Learn Go with Tests/arrays/sum_test.go
@@ -1,18 +1,37 @@
 package main
 
 import (
+	"slices"
 	"testing"
 )
 
 func TestSum(t *testing.T) {
-	t.Run("collection of any size", func(t *testing.T) {
-		numbers := []int{1, 2, 3}
+	numbers := []int{1, 2, 3}
 
-		got := Sum(numbers)
-		want := 6
+	got := Sum(numbers)
+	want := 6
 
-		if got != want {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
+	if got != want {
+		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v want %v", got, want)
 		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+	})
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
 	})
 }
