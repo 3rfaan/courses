9b96abb5ef741be7a8601f3bdcc19421893f18e8
3rfaan <zubi.arfan@gmail.com>
2025-08-29 19:23:48 +0200

    Added chapter di

---
+++ Go/Learn Go with Tests/di/di.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+}
